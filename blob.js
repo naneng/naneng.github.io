947a4069636046da03cfbed3c3e0e55594849280
Katrina Ariola <kat3ariola@gmail.com>
2023-06-17 18:27:55 -0700

    updated blob.js wave params

--- blob.js
+++ blob.js
@@ -1,49 +1,61 @@
-let blob
-
 class Blobbo {
   constructor() {
-    // lets set up the blobbo
-    this.numOfPoints = 500
-    this.time = 0
+    // Let's set up the blobbo
+    this.numOfPoints = 360;
+    this.time = 0;
   }
   
   wave(num, amp, freq, speed) {
-    return amp * sin(freq * TWO_PI * num / this.numOfPoints + this.time * speed)
+    return amp * sin(freq * TWO_PI * num / this.numOfPoints + this.time * speed);
   }
   
   draw() {
-    noStroke()
-    fill("#91fc9e")
-    beginShape()
+    noStroke();
+    fill("#7446df");
+    beginShape();
     
-    // lets draw the blobbo
+    // Let's draw the blobbo
+      
+    //   const imageDiv = select('.image');
+    //   imageDiv.width = imageDiv.offsetWidth;
+    //   imageDiv.height = imageDiv.offsetHeight;
+    //   const imageDiv = document.querySelector('.image');
+      
     for (let num = 0; num < this.numOfPoints; num = num + 1) {
-      
-      let angle = TWO_PI * num / this.numOfPoints
-      let r = 250 
-      	+ this.wave(num, 10, 3, 1) 
-      	+ this.wave(num, 15, 7, 3) 
-      	+ this.wave(num, 5, 9, 0) 
-      	+ this.wave(num, 2, 13, -5)
+      let angle = TWO_PI * num / this.numOfPoints;
+      let r = 200 
+        + this.wave(num, 10, 3, 1) 
+        + this.wave(num, 10, 7, 3) 
+        + this.wave(num, 10, 9, 0) 
+        //+ this.wave(num, 2, 13, -5);
        
-      let x = (document.querySelector('.image').offsetWidth / 2) + r * cos(angle)
-      let y = (document.querySelector('.image').offsetHeight / 2) + r * sin(angle)
+      let x = (width  / 2) + r * cos(angle);
+      let y = (height / 2) + r * sin(angle);
       
-      vertex(x, y)
+      vertex(x, y);
     }
-    endShape()
+    endShape();
     
-    this.time = this.time + 0.01
-  }
+      this.time = this.time + 0.01;
+      requestAnimationFrame(this.draw.bind(this));
+}
 }
 
 function setup() {
-  createCanvas(document.querySelector('.image').offsetWidth, document.querySelector('.image').offsetHeight)
-  blob = new Blobbo()
-  
+  const imageDiv = select('.image');
+  const canvas = createCanvas(imageDiv.width, imageDiv.height);
+  canvas.parent(imageDiv);
+  blob = new Blobbo();
 }
 
 function draw() {
-  background("#7446df")
-  blob.draw()
+//   background("#7446df");
+  blob.draw();
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  const imageDiv = select('.image');
+  const canvas = createCanvas(imageDiv.width, imageDiv.height);
+  canvas.parent(imageDiv);
+  setup();
+});
