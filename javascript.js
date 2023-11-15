06ba09c3b82ce9fc4def9789c0e16d91959ae89b
Katrina Ariola <kat3ariola@gmail.com>
2023-11-14 18:05:34 -0800

    updated html, css and javascript file

--- javascript.js
+++ javascript.js
@@ -1,44 +1,7 @@
-// header animation                >>>>>>>>>
-
-// const headerTag = document.querySelector("header");
-
-// const toggleHeader = function () {
-//   const pixels = window.pageYOffset;
-
-//   if (pixels > 60) {
-//     headerTag.classList.add("scrolled");
-//   } else {
-//     headerTag.classList.remove("scrolled");
-//   }
-// };
-
-// const fadeBox = function () {
-//   const pixels = window.pageYOffset;
-//   const alpha = Math.min(pixels / 200, 0.5);
-
-//   headerTag.style.boxShadow = `0 0 10px rgba(0, 0, 0, ${alpha})`;
-// };
-// fadeBox();
-// toggleHeader();
-
-// document.addEventListener("scroll", function () {
-//   toggleHeader();
-//   fadeBox();
-// });
-
-// marquee and animatedTag transition animations >>>>
-// function makeMarquee() {
-//   const contactTitle = "   Get in Touch  ";
-//   const marqueeText = new Array(100).fill(contactTitle).join("~");
-
-//   const marquee = document.querySelector(".contact .marquee span");
-//   marquee.innerHTML = marqueeText;
-// }
-// makeMarquee();
 
 const animatedTags = document.querySelectorAll(
   // "h1, h3, p, section img, nav, a.button, header, a.marquee, .marquee,.contact-form, a.js-scroll"
-  "section img, h2, h3, p, nav, section h1"
+  "header img, header nav, h1, .projects .project"
 );
 
 // fade out on load
@@ -78,45 +41,6 @@ window.addEventListener("resize", function () {
   fadeIn();
 });
 
-// form animations                      >>>>>>>>>
-var form = document.getElementById("my-form");
-
-async function handleSubmit(event) {
-  event.preventDefault();
-  var status = document.getElementById("my-form-status");
-  var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        status.innerHTML = "..got your message. thanks for dropping by..";
-        // status.toggleClass('open')
-        form.reset();
-      } else {
-        response.json().then((data) => {
-          if (Object.hasOwn(data, "errors")) {
-            status.innerHTML = data["errors"]
-              .map((error) => error["message"])
-              .join(", ");
-          } else {
-            status.innerHTML =
-              "Uh oh, it looks like your message got lost, try again..";
-          }
-        });
-      }
-    })
-    .catch((error) => {
-      status.innerHTML =
-        "Uh oh, it looks like your message got lost, try again..";
-    });
-}
-// form.addEventListener("submit", handleSubmit);
-
 // portfolio animations
 
 //filter animations
@@ -126,7 +50,6 @@ const filterLanguages = document.querySelectorAll(".filters-list a");
 filterLanguages.forEach((language) => {
   
   
-
   language.addEventListener("click", function (event) {
     event.preventDefault();
 
@@ -149,21 +72,3 @@ filterLanguages.forEach((language) => {
   });
 });
 
-// anime({
-//   targets: "#svg-logo path",
-//   strokeDashoffset: [anime.setDashoffset, 0],
-//   easing: "easeInOutSine",
-//   duration: 1500,
-//   delay: function (el, i) {
-//     return i * 250;
-//   },
-//   direction: "alternate",
-//   loop: true,
-// });
-
-
-
-// new fullpage('#fullpage', {
-//   autoScrolling: true,
-//   scrollHorizontally: true
-// })
